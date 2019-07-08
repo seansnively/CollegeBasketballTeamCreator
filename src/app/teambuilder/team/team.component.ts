4274c04444cfbb24921baa41e5648514d8ad3077
seansnively <gator123ss@gmail.com>
2019-07-08 11:25:39 -0400

    2 files modified

--- src/app/teambuilder/team/team.component.ts
+++ src/app/teambuilder/team/team.component.ts
@@ -11,13 +11,13 @@ export class TeamComponent implements OnInit {
   items;
   cartService1; //idk if its angular or stackblitz but without this variabale cartService methods can only be called like 10% of the time
   checkoutForm;
+  teamName;
   constructor(private cartService: CartService, private formBuilder: FormBuilder,) {
     this.cartService1 = cartService;
     this.items = this.cartService1.getItems();
 
     this.checkoutForm = this.formBuilder.group({
-      name: '',
-      address: ''
+      name: 'My Team'
     });
    }
 
@@ -27,7 +27,8 @@ export class TeamComponent implements OnInit {
   onSubmit(customerData) {
     // Process checkout data here
     console.warn('Your order has been submitted', customerData);
- 
+    this.teamName = customerData.name;
+    console.log(this.teamName);
     this.items = this.cartService1.clearCart();
     this.checkoutForm.reset();
   }
